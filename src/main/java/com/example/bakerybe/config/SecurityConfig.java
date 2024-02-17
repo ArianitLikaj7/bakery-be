bfafdaa98ecabbcb54de31dfaa3056f7b0fa45df
Arianit Likaj <arianitlikaj46@gmail.com>
2024-02-17 22:25:37 +0100

    fixed cors

--- src/main/java/com/example/bakerybe/config/SecurityConfig.java
+++ src/main/java/com/example/bakerybe/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(Customizer.withDefaults())
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
                 .exceptionHandling(ex -> ex.authenticationEntryPoint(unauthorizedHandler))
                 .authorizeHttpRequests(
                         auth -> auth
