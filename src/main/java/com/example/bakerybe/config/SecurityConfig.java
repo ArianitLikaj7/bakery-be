f458c9bd29a54896ae341a2eacc0970a84eb14a1
qendrim <qendrim.zeneli@digitzeict.com>
2024-02-18 11:24:49 +0100

    fix security configs

--- src/main/java/com/example/bakerybe/config/SecurityConfig.java
+++ src/main/java/com/example/bakerybe/config/SecurityConfig.java
@@ -3,12 +3,15 @@ package com.example.bakerybe.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
@@ -21,6 +24,10 @@ public class SecurityConfig {
 
     private final JwtAuthenticationEntryPoint unauthorizedHandler;
 
+    private final JwtAuthenticationFilter jwtAuthFilter;
+
+    private final AuthenticationProvider authenticationProvider;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -29,17 +36,22 @@ public class SecurityConfig {
                 .exceptionHandling(ex -> ex.authenticationEntryPoint(unauthorizedHandler))
                 .authorizeHttpRequests(
                         auth -> auth
-                                .requestMatchers(getSwaggerNoSecurityEndpoints())
+                                .requestMatchers(getNoAuthEndpoints())
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
-                );
+                )
+                .sessionManagement(sse -> sse.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
-    private String[] getSwaggerNoSecurityEndpoints() {
-        return new String[]{
+    private String[] getNoAuthEndpoints(){
+        return new String[] {
+                "api/v1/auth/login",
+                "api/v1/auth/refresh",
                 "/v3/api-docs/**",
                 "/v3/api-docs.yaml",
                 "/swagger-ui.html",
