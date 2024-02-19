31611ea5e789fd8f1c308557455f2c01a74c9a7a
qendrim <qendrim.zeneli@digitzeict.com>
2024-02-19 19:06:33 +0100

    set role on user creation

--- src/main/java/com/example/bakerybe/dto/UserRequest.java
+++ src/main/java/com/example/bakerybe/dto/UserRequest.java
@@ -1,10 +1,13 @@
 package com.example.bakerybe.dto;
 
+import com.example.bakerybe.entity.Role;
+
 public record UserRequest(
         String firstName,
         String lastName,
         String email,
         String password,
+        Role role,
         Long tenantId
 ) {
 }
