40ff886252913c5e810ea4b55143ea8c7444f0be
Arianit Likaj <arianitlikaj46@gmail.com>
2024-11-06 22:47:22 +0100

    fi

--- src/main/java/com/example/bakerybe/BakeryBeApplication.java
+++ src/main/java/com/example/bakerybe/BakeryBeApplication.java
@@ -18,7 +18,11 @@ public class BakeryBeApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
