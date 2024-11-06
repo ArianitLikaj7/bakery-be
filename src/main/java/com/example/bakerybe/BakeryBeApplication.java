dbd6c776783b14ec5e7e2093ab9049064b268101
Arianit Likaj <arianitlikaj46@gmail.com>
2024-11-06 21:40:52 +0100

    allow cors origin

--- src/main/java/com/example/bakerybe/BakeryBeApplication.java
+++ src/main/java/com/example/bakerybe/BakeryBeApplication.java
@@ -1,11 +1,10 @@
 package com.example.bakerybe;
 
-import com.example.bakerybe.dao.CustomUserRepository;
-import com.example.bakerybe.entity.CustomUser;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BakeryBeApplication {
@@ -14,18 +13,13 @@ public class BakeryBeApplication {
         SpringApplication.run(BakeryBeApplication.class, args);
     }
 
-//    @Bean
-//    public CommandLineRunner commandLineRunner(CustomUserRepository repository) {
-//        return args -> {
-//            var user = CustomUser.builder()
-//                    .firstName("Qendirm")
-//                    .lastName("Zeneli")
-//                    .username("qendrim")
-//                    .bakeryId(Long.valueOf(3))
-//                    .build();
-//            repository.save(user); // Save the user object to persist it in the database
-//        };
-//    }
-
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
