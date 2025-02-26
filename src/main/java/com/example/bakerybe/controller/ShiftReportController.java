f42eea20af0f0724a57f41b603fc0027ca8b5fda
Arianit Likaj <arianitlikaj46@gmail.com>
2025-02-26 21:02:07 +0100

    add v1

--- src/main/java/com/example/bakerybe/controller/ShiftReportController.java
+++ src/main/java/com/example/bakerybe/controller/ShiftReportController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/shift-reports")
+@RequestMapping("/api/v1/shift-reports")
 @RequiredArgsConstructor
 public class ShiftReportController {
 
